fc52f436ddaf09bb198f7139d59d3fe81180ee25
Sam Sloman <samsloman@live.co.uk>
2025-10-06 13:01:53 +0100

    BDOG-3466: adds service layer for inactive test repos notifications

---
+++ test/uk/gov/hmrc/healthmetrics/service/InactiveTestRepoNotifierServiceSpec.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.healthmetrics.service
+
+//import org.mockito.ArgumentMatchers.any
+//import org.mockito.Mockito.when
+//import org.scalatest.matchers.should.Matchers
+//import org.scalatest.wordspec.AnyWordSpec
+//import org.scalatestplus.mockito.MockitoSugar
+//import uk.gov.hmrc.healthmetrics.connector.TeamsAndRepositoriesConnector
+//import uk.gov.hmrc.healthmetrics.model.{RepoName, TeamName}
+//import uk.gov.hmrc.http.HeaderCarrier
+//
+//import scala.concurrent.Future
+//
+//class InactiveTestRepoNotifierServiceSpec
+//  extends AnyWordSpec
+//    with  Matchers
+//    with  MockitoSugar:
+//
+//  "InactiveTestRepoNotifierService.notify" should:
+//    "notify teams when Performance tests jobs have not been run in 360 days" in new Setup:
+//      when(teamsAndReposConnector.allTestRepos()(using any[HeaderCarrier]))
+//        .thenReturn(Future.successful(
+//          Repo(RepoName("test-repo-1"), Seq.empty, None, false, Seq(TeamName("Team 1"), TeamName("Team 2"))) ::
+//          Repo(RepoName("test-repo-2"), Seq.empty, None, false, Seq(TeamName("Team 1"), TeamName("Team 2"))) ::
+//          Nil
+//        ))
+//
+//
+//
+//  case class Setup():
+//    given HeaderCarrier = HeaderCarrier()
+//
+//    val teamsAndReposConnector = mock[TeamsAndRepositoriesConnector]
+//
+
