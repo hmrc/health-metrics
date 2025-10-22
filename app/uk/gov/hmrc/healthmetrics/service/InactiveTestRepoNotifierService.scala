1597d9cd35918bb00661cfaf9f24ee83bd84a6c7
Sam Sloman <samsloman@live.co.uk>
2025-10-22 10:00:58 +0100

    BDOG-3466: updates message with next steps

--- app/uk/gov/hmrc/healthmetrics/service/InactiveTestRepoNotifierService.scala
+++ app/uk/gov/hmrc/healthmetrics/service/InactiveTestRepoNotifierService.scala
@@ -94,7 +94,7 @@ class InactiveTestRepoNotifierService @Inject()(
 
   private def infoNotification(teamName: TeamName, testRepos: Seq[InactiveTestRepo]): SlackNotificationsConnector.Request =
     val msg = SlackNotificationsConnector.mrkdwnBlock:
-      s"Hello *${teamName.asString}*, the following test repositories may be inactive please review:"
+      s"Hello ${teamName.asString}, the following test repositories may be inactive please review:"
 
     val warnings =
       testRepos
@@ -111,11 +111,11 @@ class InactiveTestRepoNotifierService @Inject()(
     val actions =
       SlackNotificationsConnector.mrkdwnBlock(
         Seq(
-          "*Next Steps:*",
+          "*Next Steps*",
           s"• Review and decommission any inactive test repositories.",
           s"• Remove related jobs before deleting or archiving repositories.",
           s"• Keep track of your team's Test Results, visit the <https://catalogue.tax.service.gov.uk/tests?teamName=${teamName.asString}|Test Results Page> in the Catalogue."
-        ).mkString("\n")
+        ).mkString("\\n")
       )
 
     SlackNotificationsConnector.Request(
