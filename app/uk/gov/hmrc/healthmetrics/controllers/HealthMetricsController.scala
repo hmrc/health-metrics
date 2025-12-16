bde26cb0e3a6aab9fd0669e80500e36b3abc647c
MoDweik <180322631+MoDweik@users.noreply.github.com>
2025-12-16 16:17:29 +0000

    pr bot

--- app/uk/gov/hmrc/healthmetrics/controllers/HealthMetricsController.scala
+++ app/uk/gov/hmrc/healthmetrics/controllers/HealthMetricsController.scala
@@ -16,6 +16,7 @@
 
 package uk.gov.hmrc.healthmetrics.controllers
 
+import cats.implicits._
 import play.api.Logging
 import play.api.libs.json.{Json, Writes}
 import play.api.mvc.{Action, AnyContent, ControllerComponents, RequestHeader}
@@ -27,12 +28,16 @@ import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import java.time.LocalDate
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
+import uk.gov.hmrc.healthmetrics.connector.ServiceDependenciesConnector
+import uk.gov.hmrc.healthmetrics.connector.TeamsAndRepositoriesConnector
 
 @Singleton
 class HealthMetricsController @Inject()(
   cc                         : ControllerComponents
 , healthMetricsService       : HealthMetricsService
 , teamHealthMetricsRepository: TeamHealthMetricsRepository
+, serviceDependenciesConnector: ServiceDependenciesConnector
+, teamsAndRepositoriesConnector: TeamsAndRepositoriesConnector
 )(using
   ExecutionContext
 ) extends BackendController(cc) 
@@ -47,6 +52,30 @@ class HealthMetricsController @Inject()(
         .map: metrics =>
           Ok(Json.toJson(LatestHealthMetrics(metrics)))
 
+  def fake(): Action[AnyContent] =
+    Action.async: request =>
+      given RequestHeader               = request
+      val oldArtefacts               = Seq("com.typesafe.play" -> "play", "org.scala-lang" -> "scala-library")
+      val recommendedMajorJdkVersion = "21"
+
+      for      
+        reposOnOldJdk        <- serviceDependenciesConnector
+                                  .getSlugJdkVersions()
+                                  .map: repos =>
+                                    repos.filterNot(_.version.startsWith(recommendedMajorJdkVersion))
+                                  .map(_.map(_.name))
+        teamsOnOldJdk        <- teamsAndRepositoriesConnector.allRepos()
+                                  .map(_.filter(r => reposOnOldJdk.contains(r.repoName.asString)))
+                                  .map(_.flatMap(_.teamNames))
+                                .map(_.toSet)
+        teamsOnScala2OrPlay2 <- oldArtefacts.flatTraverse:
+                                  case (group, artefact) =>
+                                    serviceDependenciesConnector
+                                      .getAllProdByArtefact(group, artefact)
+                                      .map(_.flatMap(_.teamNames))
+                                .map(_.toSet)
+        teamsToNotify        = teamsOnScala2OrPlay2 ++ teamsOnOldJdk
+      yield Ok(Json.toJson(teamsOnScala2OrPlay2))
 
   def latestDigitalServiceHealthMetrics(digitalService: DigitalService): Action[AnyContent] =
     Action.async: request =>
